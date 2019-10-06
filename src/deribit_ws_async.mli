5f4311b8084020a9cc130e7b7241c8ce55165db0
Vincent Bernardoff <vb@luminar.eu.org>
2019-10-06 17:42:35 +0800

    fix compilation (fastws)

--- src/deribit_ws_async.mli
+++ src/deribit_ws_async.mli
@@ -3,8 +3,7 @@ open Async
 open Deribit_ws
 
 val connect : unit ->
-  (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t,
-  [ `Internal of exn | `WS of Fastws_async.error ]) result Deferred.t
+  (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t) Deferred.Or_error.t
 
 val connect_exn : unit ->
   (t Pipe.Reader.t * t Pipe.Writer.t * unit Deferred.t)
@@ -12,9 +11,7 @@ val connect_exn : unit ->
 
 val with_connection :
   (t Pipe.Reader.t -> t Pipe.Writer.t -> 'a Deferred.t) ->
-  ('a, [ `Internal of exn
-       | `User_callback of exn
-       | `WS of Fastws_async.error ]) result Deferred.t
+  'a Deferred.Or_error.t
 
 val with_connection_exn :
   (t Pipe.Reader.t -> t Pipe.Writer.t -> 'a Deferred.t) ->
